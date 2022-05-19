01a13faab833375a9cc40fb5f4db93761c3d3a08
Aishab0810 <aishwaryab@coreflexsolutions.com>
2022-05-19 19:23:38 +0530

    map route code

--- app/src/main/java/com/example/exploraholic/Dashboard/Info.kt
+++ app/src/main/java/com/example/exploraholic/Dashboard/Info.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.text.method.ScrollingMovementMethod
 import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.exploraholic.MainScreens.LoginScreen
 import com.example.exploraholic.R
@@ -16,8 +15,12 @@ class Info : AppCompatActivity() {
 
     private lateinit var pune_id1: TextView
     private lateinit var iv11: GifImageView
-    private lateinit var logout1: ImageView
-    private lateinit var homee1: ImageView
+    private lateinit var homee: ImageView
+    private lateinit var log_out: ImageView
+
+//    private lateinit var logout1: ImageView
+//    private lateinit var homee1: ImageView
+
     private lateinit var infm1 : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,24 +29,35 @@ class Info : AppCompatActivity() {
         pune_id1 = findViewById(R.id.pune_id)
         iv11 = findViewById(R.id.histg)
         infm1 = findViewById(R.id.infm)
-        logout1 = findViewById(R.id.log_out)
-        homee1 = findViewById(R.id.homee)
+        homee = findViewById(R.id.homee)
+        log_out = findViewById(R.id.log_out)
+
+        homee.setOnClickListener {
+            startActivity(Intent(this, Dashboard::class.java))
+        }
+
+        log_out.setOnClickListener {
+            startActivity(Intent(this, LoginScreen::class.java))
+        }
+
+//        logout1 = findViewById(R.id.log_out)
+//        homee1 = findViewById(R.id.homee)
 
         infm1.setMovementMethod(ScrollingMovementMethod())
 
 
 //        val logout1 = findViewById(R.id.log_out) as ImageView
         // set on-click listener
-        logout1.setOnClickListener {
-            startActivity(Intent(this, LoginScreen::class.java))
-            Toast.makeText(this@Info,"Logged out",Toast.LENGTH_SHORT).show()
-        }
-
-//        val home11 = findViewById(R.id.homee) as ImageView
-        // set on-click listener
-        homee1.setOnClickListener {
-            startActivity(Intent (this,Dashboard::class.java))
-            Toast.makeText(this@Info,"Dashboard",Toast.LENGTH_SHORT).show()
-        }
+//        logout1.setOnClickListener {
+//            startActivity(Intent(this, LoginScreen::class.java))
+//            Toast.makeText(this@Info,"Logged out",Toast.LENGTH_SHORT).show()
+//        }
+//
+////        val home11 = findViewById(R.id.homee) as ImageView
+//        // set on-click listener
+//        homee1.setOnClickListener {
+//            startActivity(Intent (this,Dashboard::class.java))
+//            Toast.makeText(this@Info,"Dashboard",Toast.LENGTH_SHORT).show()
+//        }
     }
 }
